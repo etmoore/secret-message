1af43ea350b847c7985ffde4f3c3e0605b82201f
etmoore <etmoore@gmail.com>
2016-09-05 22:46:29 -0600

    pass in the path containing the secret message as the first argument

--- decoder.py
+++ decoder.py
@@ -1,4 +1,10 @@
-# Make a list of all the files
+import sys
+import os
+
+# Make a list of all the files in the specified path
+encrypted_path = sys.argv[1]
+file_list = os.listdir(encrypted_path)
+
 # Iterate over each file
     # remove the numbers from the filename
     # save the new file name
