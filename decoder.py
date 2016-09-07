39e888245b7772780ea6f953a2927d673c39ec20
etmoore <etmoore@gmail.com>
2016-09-06 22:36:28 -0600

    print old and new file names

--- decoder.py
+++ decoder.py
@@ -8,6 +8,8 @@ def rename_files():
     file_list = os.listdir(encrypted_path)
     os.chdir(r"/Users/evanmoore/workspace/udacity/secret-message/prank")
     for file_name in file_list:
+        print ("Old Name: " + file_name)
+        print ("New Name: " + file_name.translate(None, '1234567890'))
         os.rename(file_name, file_name.translate(None, '1234567890'))
 
 rename_files()
